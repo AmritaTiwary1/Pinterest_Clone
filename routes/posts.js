2e4658fc0df8ab207191891858f00845aefaea00
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-12-13 23:51:54 +0530

    postmodel

--- routes/posts.js
+++ routes/posts.js
@@ -1,28 +1,14 @@
 const mongoose = require('mongoose');
 
-//Define the schema for the post model
-const postSchema = new mongoose.Schema({
-    postText: {
-        type: String,
-        required: true,
-    }, 
-    image:{
-        type:String,
-        required:true,
-    },
-    user: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now // Sets the default value to the current date and time
-    },
-    likes: {
-        type: Array, //instead of number, type is array bcoz I will store all users name who likes it,(eg.liked by sadhguru) and to show total likes - array.length
-        default: [] // Initializes likes to empty array
-    },
-});
+const postSchema = mongoose.Schema({
+  user :{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+},
+ title: String,
+ description: String,
+ image : String
+})
 
-// Create the post model and export
-module.exports = mongoose.model('Post', postSchema);
+
+module.exports = mongoose.model('post', postSchema);
